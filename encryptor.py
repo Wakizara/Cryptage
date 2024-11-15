b239550d504bfb6ef6c29eea243d881fd9ffc674
Arthur D. <160236607+Wakizara@users.noreply.github.com>
2024-11-15 17:55:48 +0100

    Create encryptor.py

---
+++ encryptor.py
@@ -0,0 +1,21 @@
+password = 'A24HJ32'
+
+alphabet_mapping = {}
+
+ensemble_1 = []
+for i in range(0, 101):
+    ensemble_1.append(i)
+
+print(ensemble_1)
+
+ensemble_2 = []
+for i in range(102, 201):
+    ensemble_2.append(i)
+
+print(ensemble_2)
+
+ensemble_3 = []
+for i in range(202, 301):
+    ensemble_3.append(i)
+
+print(ensemble_3)
