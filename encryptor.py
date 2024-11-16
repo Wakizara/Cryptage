19c24f5c23edb8c255740643981bc439141ba97b
Arthur D. <160236607+Wakizara@users.noreply.github.com>
2024-11-16 14:00:21 +0100

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -1,38 +1,36 @@
-password = 'A24HJ32'
+import string
+password = input("Enter a word you want to encrypt: ").lower()
 
-print("Let's Play")
-alphabet_mapping = {}
+print("*************Let's encrypt your password*************")
 
-ensemble_1 = []
-for i in range(0, 26):
-    ensemble_1.append(i)
+# Création de l'ensemble_1 (alphabet)
+ensemble_1 = list(string.ascii_lowercase)
 
-print(ensemble_1)
+# Création de l'ensemble_2 (0-26)
+ensemble_2 = list(range(27))
 
-ensemble_2 = []
-for i in range(0, 9):
-    ensemble_2.append(i)
+# Création de l'ensemble_3 (30-57)
+ensemble_3 = list(range(30, 58))
 
-print(ensemble_2)
+encrypted_password = []
+for char in password:
+    if char in ensemble_1:
+        # Étape 1: Trouver l'index inverse dans l'alphabet (a->z, b->y, etc.)
+        reverse_index = 25 - ensemble_1.index(char)
+        
+        # Étape 2: Convertir en nombre de ensemble_2 et soustraire 5
+        num_value = (reverse_index - 5) % 27  # Utilisation du modulo pour gérer les nombres négatifs
+        
+        # Étape 3: Multiplier par la valeur correspondante dans ensemble_3
+        final_value = num_value * ensemble_3[num_value]
+        
+        # Ajouter le résultat avec le séparateur
+        encrypted_password.append(str(final_value) + ".")
+    else:
+        # Pour les caractères qui ne sont pas des lettres
+        encrypted_password.append("_")
 
-ensemble_3 = [30]
-for i in range(202, 301):
-    ensemble_3.append(i)
-
-print(ensemble_3)
-
-encrypted_passord = []
-for letter in password:
-    if letter == password:
-        encrypted_passord += ensemble_1
-        print(f'Encrypted password: {encrypted_passord}')
-
-for number in password:
-    if number == password:
-        encrypted_passord += ensemble_2
-        print(f'Encrypted password: {encrypted_passord}')   
-
-print(f'encrypted_passord: {encrypted_passord}')
-
-
+# Joindre tous les éléments pour former le résultat final
+result = "".join(encrypted_password)
+print(f'encrypted_password: {result}')
 
